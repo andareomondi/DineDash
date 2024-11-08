6e5be12b6fbfe2976284f2cdfc2e4269e83ee96d
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-11-09 01:59:55 +0300

    test number one

--- base/views.py
+++ base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Item
 # Create your views here.
 class Home(View):
     def get(self, request):
+        items = Item.objects.all()
         context = {
-
+            'items':items,
         }
         return render(request, 'base/index.html', context=context)
