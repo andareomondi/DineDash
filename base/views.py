6f147a1572ddfe22a2dd53a0759a6666999ed203
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-11-23 03:02:52 +0300

    more changes on the files

--- base/views.py
+++ base/views.py
@@ -3,28 +3,37 @@ from django.views import View
 from .models import Product
 from django.http import JsonResponse
 from .models import *
+from django.http import HttpResponse
 # Create your views here.
 class Home(View):
     def get(self, request):
-        items = Product.objects.all()
-        if not hasattr(request.user, 'cart'):
-            cart = Cart.objects.create(user = request.user)
-            print(cart.id)
+        if request.user.is_authenticated:
+                
+            items = Product.objects.all()
+            if not hasattr(request.user, 'cart'):
+                cart = Cart.objects.create(user = request.user)
+                print(cart.id)
+            else:
+                cart = request.user.cart
+                print(cart.id)
+                # Assuming you have a cart instance
+                cart = Cart.objects.get(id=cart.id)
+                cart.update_total_price()
+                print(cart.total_price) 
+                # This will print the updated total price
+                total_unique_items = cart.total_unique_products()
+            context = {
+                'items':items,
+                'cart': cart,
+                'total_items': total_unique_items,
+            }
+            return render(request, 'base/home.html', context=context)
         else:
-            cart = request.user.cart
-            print(cart.id)
-            # Assuming you have a cart instance
-            cart = Cart.objects.get(id=cart.id)
-            cart.update_total_price()
-            print(cart.total_price) 
-             # This will print the updated total price
-            total_unique_items = cart.total_unique_products()
-        context = {
-            'items':items,
-            'cart': cart,
-            'total_items': total_unique_items,
-        }
-        return render(request, 'base/index.html', context=context)
+            items = Product.objects.all()
+            context = {
+                'items':items
+            }
+            return render(request, 'base/home2.html', context=context)
     def post(self, request):
         value = request.POST.get('value')
         cart = Cart.objects.get(id=cart.id)
